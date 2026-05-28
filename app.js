0178899a68c5d48c17f31261b5663a467690d0e4
Nikos Gavalakis <nikosgavalakis@gmail.com>
2026-05-28 10:41:34 +0300

    Add files via upload
    Third commit

--- app.js
+++ app.js
@@ -1,5 +1,53 @@
+import { initializeApp }
+
+from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
+
+import {
+
+getAuth,
+GoogleAuthProvider,
+signInWithPopup,
+signOut,
+onAuthStateChanged
+
+}
+
+from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+
+
+
+// FIREBASE CONFIG
+
+const firebaseConfig = {
+
+apiKey: "AIzaSyDXJWygGmkLERGCv1nFj9H7xHWVYuF-Row",
+
+authDomain: "medical-logbook-9a47f.firebaseapp.com",
+
+projectId: "medical-logbook-9a47f",
+
+storageBucket: "medical-logbook-9a47f.firebasestorage.app",
+
+messagingSenderId: "830569350254",
+
+appId: "1:830569350254:web:32635a572746bc4b6b127f"
+
+};
+
+
+// INITIALIZE FIREBASE
+
+const app =
+initializeApp(firebaseConfig);
+
+const auth =
+getAuth(app);
+
 const provider =
 new GoogleAuthProvider();
+
+
+// LOGIN
 
 window.login = async function(){
 
@@ -12,20 +60,32 @@ const user = result.user;
 
 console.log(user);
 
+alert("Συνδέθηκε:");
+
 }
 catch(error){
 
 console.error(error);
 
+alert(error.message);
+
 }
 
 }
+
+
+// LOGOUT
 
 window.logout = async function(){
 
 await signOut(auth);
 
+alert("Αποσυνδέθηκε");
+
 }
+
+
+// USER STATE
 
 onAuthStateChanged(auth, user => {
 
@@ -51,26 +111,3 @@ document.getElementById("userBox")
 }
 
 });
-
-
-
-
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
-
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDXJWygGmkLERGCv1nFj9H7xHWVYuF-Row",
-    authDomain: "medical-logbook-9a47f.firebaseapp.com",
-    projectId: "medical-logbook-9a47f",
-    storageBucket: "medical-logbook-9a47f.firebasestorage.app",
-    messagingSenderId: "830569350254",
-    appId: "1:830569350254:web:32635a572746bc4b6b127f"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-</script>
