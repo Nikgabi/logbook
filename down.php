c0f305d79066953b5f041a6f449dc77e74f6a9e5
Nikos Gavalakis <nikosgavalakis@gmail.com>
2026-06-12 12:05:49 +0300

    Delete down.php

--- down.php
+++
@@ -1,9 +0,0 @@
-</div>
-</div>
-
-
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
-<script type="module" src="./js/app.js"></script>
-</body>
-</html>
