870963d6d7a47a1c138432c48e7aba4ad857f999
Nikos Gavalakis <nikosgavalakis@gmail.com>
2026-06-19 13:04:06 +0300

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -41,6 +41,10 @@ export const auth = getAuth(app);
 
 const provider = new GoogleAuthProvider();
 
+provider.setCustomParameters({
+  prompt: 'select_account'
+});
+
 let currentUser = null;
 
 export async function login(){
